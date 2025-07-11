b7e452e753066f7faabf7e83a3352b39d0684512
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2025-07-11 02:31:46 +0100

    Implement contract constructor and deploy in test env

--- contracts/Savings.sol
+++ contracts/Savings.sol
@@ -7,6 +7,22 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Savings {
   uint256 public deadline;
-  address public recipient;
+  address public recipientAddress;
   address public owner;
+
+  constructor(uint256 _deadline, address _recipientAddress, address _owner) payable {
+    require(msg.value >= 0.01 ether, "Must deploy with at least 0.01 ETH");
+    require(block.timestamp < _deadline, "Deadline must be in the future");
+
+    deadline = _deadline;
+    recipientAddress = _recipientAddress;
+    owner = _owner;
+  }
+
+  modifier onlyOwner {
+    require(msg.sender == owner, "Only owner can perform this function");
+    _;
+  }
+
+  
 }
--- test/Savings.js
+++ test/Savings.js
@@ -6,8 +6,29 @@ describe('Savings', () => {
   async function deployContractAndVariables() {
     const [deployer, secondUser] = await ethers.getSigners()
 
+    console.log("ETH BAL", await ethers.provider.getBalance(deployer))
+
+    const date = Date.now()
+    // 60 seconds, 60 minutes (1 hours), 24 hours
+    // 30 days, 3 months
+    const deadlineInSeconds = (60 * 60 * 24 * 30 * 3)
+    const deadlineDate = date + (deadlineInSeconds * 1000)
+
     const Token = await ethers.getContractFactory('TestERC20');
     const token = await Token.deploy("TestERC20", "TET")
+
+    const Contract = await ethers.getContractFactory('Savings');
+    const contract = await Contract.deploy(
+      deadlineDate,
+      secondUser.address,
+      deployer.address,
+      {value: ethers.parseEther("0.01")}
+    )
+
+    await contract.waitForDeployment()
+
+    console.log("ETH BAL", await ethers.provider.getBalance(deployer))
+    console.log("Contract BAL", ethers.formatEther(await ethers.provider.getBalance(contract)))
 
     return { deployer, token, secondUser }
   }
@@ -15,13 +36,13 @@ describe('Savings', () => {
   it("initialise", async function() {
     const { deployer, token, secondUser } = await loadFixture(deployContractAndVariables)
 
-    console.log(await token.getAddress())
-    let balance = await token.balanceOf(deployer.address)
-    console.log(ethers.formatEther(balance))
-    const tx = await token.transfer(secondUser.address, ethers.parseEther("100"))
-    tx.wait()
-    console.log("Balance", ethers.formatEther((await token.balanceOf(deployer.address))))
-    console.log("sec acct", ethers.formatEther(await token.balanceOf(secondUser.address)))
+    // console.log(await token.getAddress())
+    // let balance = await token.balanceOf(deployer.address)
+    // console.log(ethers.formatEther(balance))
+    // const tx = await token.transfer(secondUser.address, ethers.parseEther("100"))
+    // tx.wait()
+    // console.log("Balance", ethers.formatEther((await token.balanceOf(deployer.address))))
+    // console.log("sec acct", ethers.formatEther(await token.balanceOf(secondUser.address)))
     console.log("fanny", deployer.address)
   })
 })
